4d1627125554c144af8860db3ccca703bf1b3720
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-27 21:37:40 -0300

    test: create a method to mount product card component

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -2,6 +2,19 @@ import { mount } from '@vue/test-utils';
 import ProductCard from './ProductCard';
 import { makeServer } from '@/miragejs/server';
 
+const mountProductCard = (server) => {
+  return mount(ProductCard, {
+    propsData: {
+      product: server.create('product', {
+        title: 'Relógio bonito',
+        price: '22.00',
+        image:
+          'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+      }),
+    },
+  });
+};
+
 describe('ProductCard - unit', () => {
   let server;
   beforeEach(() => {
@@ -13,29 +26,13 @@ describe('ProductCard - unit', () => {
   });
 
   it('should match snapshot', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-          image:
-            'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-        }),
-      },
-    });
+    const wrapper = mountProductCard(server);
 
     expect(wrapper.element).toMatchSnapshot();
   });
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '22.00',
-        }),
-      },
-    });
+    const wrapper = mountProductCard(server);
 
     expect(wrapper.vm).toBeDefined();
     expect(wrapper.text()).toContain('Relógio bonito');
