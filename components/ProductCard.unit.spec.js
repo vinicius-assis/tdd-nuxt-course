67473e237c292aeb7b23aa18e0f1630130e76bd2
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-27 21:29:53 -0300

    test: ensure product card have title and price as properties

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,14 +1,29 @@
 import { mount } from '@vue/test-utils';
 import ProductCard from './ProductCard';
+import { makeServer } from '@/miragejs/server';
 
 describe('ProductCard - unit', () => {
+  let server;
+  beforeEach(() => {
+    server = makeServer({ enviroment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should mount the component', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'Relógio bonito',
+          price: '22.00',
+        }),
       },
     });
 
     expect(wrapper.vm).toBeDefined();
+    expect(wrapper.text()).toContain('Relógio bonito');
+    expect(wrapper.text()).toContain('22.00');
   });
 });
