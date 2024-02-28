91ba654259878bfc446ab60f378e8a0c1d4f33b2
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-02-27 21:31:06 -0300

    test: create snapshot to ensure the component structure

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -12,6 +12,21 @@ describe('ProductCard - unit', () => {
     server.shutdown();
   });
 
+  it('should match snapshot', () => {
+    const wrapper = mount(ProductCard, {
+      propsData: {
+        product: server.create('product', {
+          title: 'Relógio bonito',
+          price: '22.00',
+          image:
+            'https://images.unsplash.com/photo-1495856458515-0637185db551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        }),
+      },
+    });
+
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
   it('should mount the component', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
