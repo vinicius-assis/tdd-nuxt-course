edec96588ab9fd4c30cb610faecf0011595116fa
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-02 20:34:52 -0300

    test: ensure mount the ProductCard component 10 times

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -1,13 +1,25 @@
 import { mount } from '@vue/test-utils';
 import axios from 'axios';
+import { nextTick } from 'vue';
 import ProductList from '.';
 import Search from '@/components/Search';
+import ProductCard from '@/components/ProductCard';
+import { makeServer } from '@/miragejs/server';
 
 jest.mock('axios', () => ({
   get: jest.fn(),
 }));
 
 describe('ProductList - integration', () => {
+  let server;
+  beforeEach(() => {
+    server = makeServer({ enviroment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should mount the component', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.vm).toBeDefined();
@@ -27,5 +39,23 @@ describe('ProductList - integration', () => {
 
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(axios.get).toHaveBeenCalledWith('/api/products');
+  });
+
+  it('should mount the ProductCard component 10 times', async () => {
+    const products = server.createList('product', 10);
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await nextTick();
+
+    const cards = wrapper.findAllComponents(ProductCard);
+
+    expect(cards).toHaveLength(10);
   });
 });
