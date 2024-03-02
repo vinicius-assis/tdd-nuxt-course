7c62e6ee71b0d00f8cfc8c92f1bcc2ca73928378
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-02 20:27:37 -0300

    test: ensure axios get is called on component mount

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -1,6 +1,11 @@
 import { mount } from '@vue/test-utils';
+import axios from 'axios';
 import ProductList from '.';
 import Search from '@/components/Search';
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+}));
 
 describe('ProductList - integration', () => {
   it('should mount the component', () => {
@@ -11,5 +16,16 @@ describe('ProductList - integration', () => {
   it('should mount the Search component as a child', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.findComponent(Search)).toBeDefined();
+  });
+
+  it('should call axios.get on component mount', () => {
+    mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith('/api/products');
   });
 });
