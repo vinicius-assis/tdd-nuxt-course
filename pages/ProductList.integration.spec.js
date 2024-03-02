91459d320851b973f1fb425faa7ede05b6740faf
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-02 20:19:03 -0300

    test: ensure Search is mount as a child

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -1,9 +1,15 @@
 import { mount } from '@vue/test-utils';
 import ProductList from '.';
+import Search from '@/components/Search';
 
 describe('ProductList - integration', () => {
   it('should mount the component', () => {
     const wrapper = mount(ProductList);
     expect(wrapper.vm).toBeDefined();
+  });
+
+  it('should mount the Search component as a child', () => {
+    const wrapper = mount(ProductList);
+    expect(wrapper.findComponent(Search)).toBeDefined();
   });
 });
