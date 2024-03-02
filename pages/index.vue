dc660b296cd51c0a85d62b6186a134d100085344
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-02 20:56:07 -0300

    feat: implement filter functionality in product list

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="my-8">
-    <search />
+    <search @doSearch="setSearchTerm" />
     <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
       <span class="mt-3 text-sm text-gray-500">200+ Products</span>
@@ -8,7 +8,7 @@
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
         <product-card
-          v-for="product in products"
+          v-for="product in list"
           :key="product.index"
           :product="product"
         />
@@ -27,7 +27,18 @@ export default {
     return {
       products: [],
       errorMessage: '',
+      filterTerm: '',
     };
+  },
+  computed: {
+    list() {
+      if (this.filterTerm !== '') {
+        return this.products.filter(({ title }) =>
+          title.includes(this.filterTerm)
+        );
+      }
+      return this.products;
+    },
   },
   async created() {
     try {
@@ -35,6 +46,11 @@ export default {
     } catch (error) {
       this.errorMessage = 'Problemas ao carregar a lista!';
     }
+  },
+  methods: {
+    setSearchTerm({ term }) {
+      this.filterTerm = term;
+    },
   },
 };
 </script>
