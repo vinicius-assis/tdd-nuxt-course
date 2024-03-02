1dcc2c7ad3291e8f898fe9ce0d8d5ba4da032418
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-02 19:50:31 -0300

    test: ensure search event is emit

--- components/Search.unit.spec.js
+++ components/Search.unit.spec.js
@@ -6,4 +6,16 @@ describe('Search - unit', () => {
     const wrapper = mount(Search);
     expect(wrapper.vm).toBeDefined();
   });
+
+  it('should emit search event when form is submitted', async () => {
+    const wrapper = mount(Search);
+    const term = 'termo para busca';
+
+    await wrapper.find('input[type="search"]').setValue(term);
+    await wrapper.find('form').trigger('submit');
+
+    expect(wrapper.emitted().doSearch).toBeTruthy();
+    expect(wrapper.emitted().doSearch.length).toBe(1);
+    expect(wrapper.emitted().doSearch[0]).toEqual([{ term }]);
+  });
 });
