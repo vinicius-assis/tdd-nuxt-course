a6e1736e24dc15791358be1e815ffc82dc52bff2
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-03 19:40:32 -0300

    feat: add cart state as nuxt plugin

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -31,7 +31,6 @@
 </template>
 
 <script>
-import { cartState } from '@/state';
 export default {
   name: 'ProductCard',
   props: {
@@ -42,8 +41,8 @@ export default {
   },
   methods: {
     addToCart() {
-      cartState.open = true;
-      cartState.items.push(this.product);
+      this.$cart.open();
+      this.$cart.addProduct(this.product);
     },
   },
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/miragejs.js'],
+  plugins: ['@/plugins/miragejs.js', '@/plugins/cart.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: false,
