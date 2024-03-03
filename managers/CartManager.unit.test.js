18e939a797a47949ed2f0e454fbfb62e73b93c57
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-03 18:38:57 -0300

    test: create managers todo tests

---
+++ managers/CartManager.unit.test.js
@@ -0,0 +1,17 @@
+describe('CartManager', () => {
+  it.todo('should set cart to open');
+
+  it.todo('should set cart to closed');
+
+  it.todo('should add product to the card only once');
+
+  it.todo('should remove product from cart');
+
+  it.todo('should clear products');
+
+  it.todo('should clear cart');
+
+  it.todo('should return true if cart is not empty');
+
+  it.todo('should return true if product is already in the cart');
+});
