291fcbcd755f8b644dc03a189cda8f7ce5d0ffe1
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-03 18:47:08 -0300

    test: ensure add product the card onyl once

--- managers/CartManager.unit.test.js
+++ managers/CartManager.unit.test.js
@@ -1,21 +1,39 @@
 import { CartManager } from '@/managers/CartManager';
+import { makeServer } from '@/miragejs/server';
 
 describe('CartManager', () => {
+  let server;
+  let manager;
+
+  beforeEach(() => {
+    manager = new CartManager();
+    server = makeServer({ enviroment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should set cart to open', () => {
-    const manager = new CartManager();
     const state = manager.open();
 
     expect(state.open).toBe(true);
   });
 
   it('should set cart to closed', () => {
-    const manager = new CartManager();
     const state = manager.close();
 
     expect(state.open).toBe(false);
   });
 
-  it.todo('should add product to the card only once');
+  it('should add product to the card only once', () => {
+    const product = server.create('product');
+
+    manager.addProduct(product);
+    const state = manager.addProduct(product);
+
+    expect(state.items).toHaveLength(1);
+  });
 
   it.todo('should remove product from cart');
 
