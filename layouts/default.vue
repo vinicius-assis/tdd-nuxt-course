08f10158ea2bc0d4463595d287e482140c23cb6b
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-03 19:44:35 -0300

    test: create test to check if default layout is works right

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,6 +32,7 @@
           </div>
           <div class="flex items-center justify-end w-full">
             <button
+              data-testid="toggle-button"
               class="text-gray-600 focus:outline-none mx-4 sm:mx-0"
               @click="toggleCart"
             >
@@ -114,21 +115,24 @@
 
 <script>
 import Cart from '@/components/Cart';
-import { cartState } from '@/state';
 
 export default {
   components: { Cart },
   computed: {
     isCartOpen() {
-      return cartState.open;
+      return this.$cart.getState().open;
     },
     products() {
-      return cartState.items;
+      return this.$cart.getState().items;
     },
   },
   methods: {
     toggleCart() {
-      cartState.open = !cartState.open;
+      if (this.$cart.getState().open) {
+        this.$cart.close();
+      } else {
+        this.$cart.open();
+      }
     },
   },
 };
