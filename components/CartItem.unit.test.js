2d8ccc911ec69ac57f68ad486f6b94843f05c661
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-03 17:37:05 -0300

    refactor: create mountCardItem method to test

--- components/CartItem.unit.test.js
+++ components/CartItem.unit.test.js
@@ -1,9 +1,35 @@
 import { mount } from '@vue/test-utils';
 import CartItem from '@/components/CartItem';
+import { makeServer } from '@/miragejs/server';
+
+let server;
+
+const mountCartItem = () => {
+  const product = server.create('product', {
+    title: 'Lindo Relógio',
+    price: '22.33',
+  });
+
+  const wrapper = mount(CartItem, {
+    propsData: {
+      product,
+    },
+  });
+
+  return { wrapper, product };
+};
 
 describe('CartItem - unit', () => {
+  beforeEach(() => {
+    server = makeServer({ enviroment: 'test' });
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
   it('should mount the component', () => {
-    const wrapper = mount(CartItem);
+    const { wrapper } = mountCartItem();
 
     expect(wrapper.vm).toBeDefined();
   });
