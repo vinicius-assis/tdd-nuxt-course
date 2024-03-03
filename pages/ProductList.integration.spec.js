bf682cec863b377a1eda5c46dfdf1bcdca5e5d5f
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-02 21:00:38 -0300

    test: ensure display all product when the search term is clearned

--- pages/ProductList.integration.spec.js
+++ pages/ProductList.integration.spec.js
@@ -101,4 +101,33 @@ describe('ProductList - integration', () => {
     expect(wrapper.vm.filterTerm).toEqual('relógio');
     expect(cards).toHaveLength(2);
   });
+
+  it('should display all product when the search term is clearned', async () => {
+    const products = [
+      ...server.createList('product', 10),
+      server.create('product', {
+        title: 'Meu relógio amado',
+      }),
+    ];
+
+    axios.get.mockReturnValue(Promise.resolve({ data: { products } }));
+
+    const wrapper = mount(ProductList, {
+      mocks: {
+        $axios: axios,
+      },
+    });
+
+    await nextTick();
+
+    const search = wrapper.findComponent(Search);
+    search.find('input[type="search"]').setValue('relógio');
+    await search.find('form').trigger('submit');
+    search.find('input[type="search"]').setValue('');
+    await search.find('form').trigger('submit');
+
+    const cards = wrapper.findAllComponents(ProductCard);
+    expect(wrapper.vm.filterTerm).toEqual('');
+    expect(cards).toHaveLength(11);
+  });
 });
