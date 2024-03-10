fbd64f3922c569629a7c871c4e2ace26ca6923ad
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-09 23:29:25 -0300

    feat: add remove product button

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -8,6 +8,7 @@
       />
       <div class="mx-3">
         <h3 class="text-sm text-gray-600">{{ product.title }}</h3>
+        <button data-testid="remove-button">remover</button>
         <div class="flex items-center mt-2">
           <button
             data-testid="-"
