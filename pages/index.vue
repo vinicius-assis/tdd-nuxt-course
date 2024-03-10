5ec0c4c6fb4b767d4c7547b14992bd1194b39637
Vinicius Assis <viniciusassis.dev@hotmail.com>
2024-03-09 23:51:37 -0300

    feat: add quantity label to show product quantity

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,11 @@
     <search @doSearch="setSearchTerm" />
     <div v-if="errorMessage === ''" class="container mx-auto px-6">
       <h3 class="text-gray-700 text-2xl font-medium">Wrist Watch</h3>
-      <span class="mt-3 text-sm text-gray-500">200+ Products</span>
+      <span
+        class="mt-3 text-sm text-gray-500"
+        data-testid="total-quantity-label"
+        >{{ quantityLabel }}</span
+      >
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
@@ -38,6 +42,13 @@ export default {
         );
       }
       return this.products;
+    },
+    quantityLabel() {
+      const quantity = this.list.length;
+      if (quantity === 1) {
+        return '1 Product';
+      }
+      return `${quantity} Products`;
     },
   },
   async created() {
